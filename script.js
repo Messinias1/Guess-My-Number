05252da7f745001e0122bd4a4ef7612387232020
Messinias1 <you@example.com>
2022-06-15 23:10:29 -0400

    another change on pc to test

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 // http://127.0.0.1:5500/index.html
 
 console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.btn again').textContent);
 console.log('Hello');
 console.log('Hello');
