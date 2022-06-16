abc24a383319e5ec59156c066d9495493e069975
Messinias1 <you@example.com>
2022-06-15 23:07:34 -0400

    testing from pc

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 // http://127.0.0.1:5500/index.html
 
-
-console.log(document.querySelector('.message').textContent)
-console.log('Hello')
+console.log(document.querySelector('.message').textContent);
+console.log('Hello');
+console.log('Hello');
