819542b612d07ce599ba6115a71051062c971973
Messinias1 <Messinias1@gmail.com>
2022-06-18 19:04:20 -0400

    query selectors

--- script.js
+++ script.js
@@ -2,6 +2,4 @@
 // http://127.0.0.1:5500/index.html
 
 console.log(document.querySelector('.message').textContent);
-console.log(document.querySelector('.btn again').textContent);
-console.log('Hello');
-console.log('Hello');
+document.querySelector('.message').textContent = "🎉 Correct Number!"
