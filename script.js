ac27c547f5e8be0510a8d15b9e8824c0a4b10693
Messinias1 <you@example.com>
2022-06-21 22:11:47 -0400

    play with .value

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 // http://127.0.0.1:5500/index.html
 
 console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = "🎉 Correct Number!"
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 1;
+
+document.querySelector('.guess').value = 13;
